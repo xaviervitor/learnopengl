89fddb17dd1dd5c5f744b02ecbe41fa3527621ee
Vitor Xavier <xavier1vitor@gmail.com>
2023-08-29 13:26:05 -0300

    Lighting Maps chapter

--- resources/shaders/lighting.fs
+++ resources/shaders/lighting.fs
@@ -1,9 +1,8 @@
 #version 330 core
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
     float shininess;
 };
 
@@ -14,45 +13,33 @@ struct Light {
     vec3 specular;
 };
 
-// interpolated position and normal vector. will be used
-// in the lighting calculations.
 in vec3 FragPos;
 in vec3 Normal;
+in vec2 TexCoords;
 
-// object and light data are stored in Material and Light
-// structs. 
 uniform Material material;
 uniform Light light;
-
 uniform vec3 viewPos;
 
 out vec4 FragColor;
 
 void main() {
-    // ambient
-    // the previously used "ambient strength" is now defined as
-    // the light.ambient component, as a full color.
-    vec3 ambient = light.ambient * (material.ambient);
-
     // diffuse
     vec3 normal = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(normal, lightDir), 0.0);
-    // previously, light.diffuse was just 1.0f, and ommited.
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    // the previously implemented "specular strength" is now defined 
-    // as the light.specular component, as a full color.
-    vec3 specular = light.specular * (spec * material.specular);
 
-    // the final multiplication by the "object color" doesn't need to
-    // be done anymore because all the color components of the material
-    // are already multiplied in each lighting component. 
-    vec3 result = ambient + diffuse + specular;
+    // instead of using constants, we sample each color from diffuse and
+    // specular textures. the ambient color is the same as the diffuse
+    // value. the specular tipically have only B&W colors to appear realistic.
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse = light.diffuse * (diff * vec3(texture(material.diffuse, TexCoords)));
+    vec3 specular = light.specular * (spec * vec3(texture(material.specular, TexCoords)));
 
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- resources/shaders/lighting.vs
+++ resources/shaders/lighting.vs
@@ -1,29 +1,20 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
-
-// the normal vector is passed to the vertex shader
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-// the interpolated position and normal vector of the fragment
-// need to be passed in an out variable
 out vec3 FragPos;
 out vec3 Normal;
+out vec2 TexCoords;
 
 void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    
-    // the position and normal vectors need to be in world
-    // space. a simple model matrix multiplication is enough for
-    // the position vector.
+    gl_Position = projection * view * model * vec4(aPos, 1.0);    
     FragPos = vec3(model * vec4(aPos, 1.0));
-    // the normal vector needs to account for the non-uniform scaling
-    // of the model matrix. to transform a normal to world space,
-    // we use a "normal matrix", the transpose of the inverse of the
-    // upper-left 3x3 part of the model matrix.
     Normal = mat3(transpose(inverse(model))) * aNormal;
+    TexCoords = aTexCoords;
 }
